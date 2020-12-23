bcf74b757d403f8f8fd0522c66b0ebaba12c26d5
DEXTEREAGLE <degman@ukr.net>
2020-12-23 10:20:44 +0200

    Script completed, wait for test

--- LPIC2020.sh
+++ LPIC2020.sh
@@ -3,5 +3,6 @@
 # Завдання: Загрузить файл с веб сервера и отобразить его первые 11 строк 
 echo "Введіть посилання для скачування файлу" 
 read path 
-
-wget $path 
+filename="example.txt" 
+wget -O $path $filename
+cat $filename | sed -n 1,11p
